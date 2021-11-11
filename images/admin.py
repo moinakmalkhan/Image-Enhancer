888110b8df1a0ce8272af71779704fae33f7b25a
Moin Khan <moinakmalkhan@gmail.com>
2021-11-11 16:29:01 +0500

    Adding gitignore file

--- images/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Image
-# Register your models here.
-@admin.register(Image)
-class ImageModelAdmin(admin.ModelAdmin):
-    pass
--- images/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'image'
-
-urlpatterns = [
-    path('get/<int:pk>', views.stream_file, name='get_image'),
-
-]
